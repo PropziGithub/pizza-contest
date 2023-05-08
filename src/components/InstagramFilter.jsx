433ae41faf9ffd4a8926ae81c90a571545169f6c
Raphael Limbikani Mbewe <rmbewe68@gmail.com>
2023-05-08 07:21:54 +0300

    Feat: add Instagram Filters

--- src/components/InstagramFilter.jsx
+++ src/components/InstagramFilter.jsx
@@ -4,13 +4,18 @@ import three from "@/assets/three.gif";
 
 const InstagramFilters = () => {
   return (
-    <div className="mt-20 w-[486px] bg-primary-medium rounded-[20px] border border-1 border-grey-light py-4 md:px-10">
+    <a
+      href="https://www.instagram.com/ar/776780867244361"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="mt-20 w-[486px] bg-primary-medium rounded-[20px] border border-1 border-grey-light py-4 md:px-10 cursor-pointer"
+    >
       <div className="flex  justify-around items-center px-4 md:px-0">
         <div className="md:hidden flex justify-center items-center h-[47px] w-[47px] bg-primary-medium rounded-full flex-none ">
           <img src={three} alt="first step" className="h-[30px]   mx-auto" />
         </div>
         <div className="">
-          <img src={filter} alt="instagram filters"/>
+          <img src={filter} alt="instagram filters" />
         </div>
       </div>
       <div className="text-[#FFFFFF] text-[.8rem] px-10 md:px-0">
@@ -19,7 +24,7 @@ const InstagramFilters = () => {
         our page highlights for customers to view, like, and comment on your
         special recipe.
       </div>
-    </div>
+    </a>
   );
 };
 
